5b7d3085c364641b233880617e5263a2ef469e60
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2024-08-23 00:23:00 -0300

    Prepare to deploy

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { app } from "./app";
 app
   .listen({
     port: env.PORT,
+    host: env.NODE_ENV === "development" ? "localhost" : "0.0.0.0",
   })
   .then(() => {
     console.log("HTTP Server Running!");
